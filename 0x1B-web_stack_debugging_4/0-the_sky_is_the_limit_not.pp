27b747e0d7274f584cee01ec877aa23f05d8d34e
Dare Oloruntoba <64743870+dretoba91@users.noreply.github.com>
2022-12-07 22:30:39 +0100

    0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,1 +1,5 @@
-q
+# Fixing the number of failed requests down to 0
+exec { 'fix--for-nginx':
+  command => "sed -i 's/worker_processes 4;/worker_processes 7;/g' /etc/nginx/nginx.conf; sudo service nginx restart",
+  path    => ['/bin', '/usr/bin', '/usr/sbin']
+}
